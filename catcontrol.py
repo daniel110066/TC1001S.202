92f161d7b72d53677af1fa26013457f8b3a78aac
coolestKitten <emmanuel@Catcomputer.local>
2022-09-13 10:04:34 -0500

    codigo agregado a catcontrol

--- catcontrol.py
+++ catcontrol.py
@@ -1,1 +1,24 @@
 ## catcontrol
+
+import random
+import time
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+
+solution = 'gato control' #end goal
+start = '' #starting string
+holder = ''# letter holder to pop
+i = 0
+
+while start != solution:
+    holder = random.choice(alphabet)
+    if holder != solution[i]:
+        alphabet.remove(holder)
+        print(start + holder)
+        time.sleep(.05)
+    else:
+        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+        start += holder
+        print(start)
+        i += 1
+        time.sleep(.05)
