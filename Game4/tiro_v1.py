98e4824e7067bbc2b3883342e32282e86da63032
coolestKitten <sepcaro2@gmail.com>
2022-09-14 19:50:38 -0500

    projectiles and enemies in game four move faster. enemies reaching the end of the screen no longer crashes the game

---
+++ Game4/tiro_v1.py
@@ -0,0 +1,75 @@
+from random import randrange
+from turtle import *
+from freegames import vector
+
+ball = vector(-200, -200)
+speed = vector(0, 0)
+targets = []
+
+def tap(x, y):
+    "Respond to screen tap."
+    if not inside(ball):
+        ball.x = -199
+        ball.y = -199
+        ## these numbers control the speed of the red ball
+        speed.x = (x + 200) / 13
+        speed.y = (y + 200) / 13
+
+def inside(xy):
+    "Return True if xy within screen."
+    return -200 < xy.x < 200 and -200 < xy.y < 200
+
+def draw():
+    "Draw ball and targets."
+    clear()
+
+    for target in targets:
+        goto(target.x, target.y)
+        dot(20, 'blue')
+
+    if inside(ball):
+        goto(ball.x, ball.y)
+        dot(6, 'red')
+
+    update()
+
+def move():
+    "Move ball and targets."
+    if randrange(40) == 0:
+        y = randrange(-150, 150)
+        target = vector(200, y)
+        targets.append(target)
+
+    ## this for statements controls the speed of the blue balls
+    ## should be no higher than 3
+    for target in targets:
+        target.x -= 2.5
+
+    if inside(ball):
+        speed.y -= 0.35
+        ball.move(speed)
+
+    dupe = targets.copy()
+    targets.clear()
+
+    for target in dupe:
+        if abs(target - ball) > 13:
+            targets.append(target)
+
+    draw()
+
+    for target in targets:
+        if not inside(target):
+            goto(target.x, target.y)
+            dot(20, 'blue')
+            
+
+    ontimer(move, 50)
+
+setup(420, 420, 370, 0)
+hideturtle()
+up()
+tracer(False)
+onscreenclick(tap)
+move()
+done()
